3011f39262598bba9eec5ef6814adb6f7caff94e
Zylvian <33404765+Zylvian@users.noreply.github.com>
2019-04-12 04:37:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
   download_url = 'https://github.com/Zylvian/py_googlesheets_grading/archive/0.11.tar.gz',    # I explain this later on
   keywords = ['grading','google','sheets','gsheets'],   # Keywords that define your package best
   install_requires=[  # I get to this in a second
+          'gitpython',
           'pygsheets',
-          'Repo',
-          'gitpython'
+          'Repo'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
