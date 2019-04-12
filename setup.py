0d011417adc3db89d33710d3884b85b4c230b733
Zylvian <jovlisen@gmail.com>
2019-04-12 04:24:09 +0200

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'py_googlesheets_grading',         # How you named your package folder (MyLib)
   packages = ['py_googlesheets_grading'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.11',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Grades Gitlab stuff for you lmao',   # Give a short description about your library
   author = 'Jarle H. Wallevik',                   # Type in your name
@@ -12,7 +12,8 @@ setup(
   keywords = ['grading','google','sheets','gsheets'],   # Keywords that define your package best
   install_requires=[  # I get to this in a second
           'pygsheets',
-          'Repo'
+          'Repo',
+          'gitpython'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
