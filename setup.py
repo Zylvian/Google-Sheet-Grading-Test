0fe575e5caa78409573e493e07f3c3eb6259eb28
Zylvian <jovlisen@gmail.com>
2019-04-12 04:32:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Jarle H. Wallevik',                   # Type in your name
   author_email = 'jovlisen@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Zylvian/py_googlesheets_grading',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Zylvian/py_googlesheets_grading/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Zylvian/py_googlesheets_grading/archive/0.11.tar.gz',    # I explain this later on
   keywords = ['grading','google','sheets','gsheets'],   # Keywords that define your package best
   install_requires=[  # I get to this in a second
           'pygsheets',
